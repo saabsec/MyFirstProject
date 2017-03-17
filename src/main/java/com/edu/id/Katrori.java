102e456dc1f8d96f97e16b9c49e6cbab50841c32
Hunahau <mgrabova@gmail.com>
2017-03-17 20:26:07 +0100

    Krijova klasen Katrori edhe metoden gjejPerimetrin.

---
+++ src/main/java/com/edu/id/Katrori.java
@@ -0,0 +1,19 @@
+package com.edu.id;
+
+public class Katrori {
+  
+  private Katrori() {
+    // Constructor.
+  }
+  
+  /**
+   * Ky funksion sherben per te gjetur Perimetrin.
+   * 
+   * @param brinja
+   * @return
+   */
+  public static int gjejPerimetrinKubit(int brinja) {
+    return brinja * 4;
+  }
+  
+}
