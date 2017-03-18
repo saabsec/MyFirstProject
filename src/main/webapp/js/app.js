a6afd1f35fe233c4c177ceeb1e34eb94be193d88
Hunahau <mgrabova@gmail.com>
2017-03-18 16:50:22 +0100

    ngroute

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,6 +1,20 @@
 (function() {
 	'use strict';
 	
-	angular.module('myNew', []);
+	angular.module('myNew', [ 'ngRoute' ]).controller('MainCtrl',
+			(function($scope) {
+				console.log('MainCtrl');
+				
+				$scope.shfaqtekst = false;
+				
+				$scope.aktivizotekst = function() {
+					if (!$scope.shfaqtekst) {
+						$scope.shfaqtekst = true;
+					} else {
+						$scope.shfaqtekst = false;
+					}
+				};
+				
+			}));
 	
 })();
